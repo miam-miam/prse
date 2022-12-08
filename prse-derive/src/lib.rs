6ba7037f0c015985692cf8965c82cfdf2e4764ac
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2022-12-08 17:41:04 +0000

    Add Licenses, CI and a way to parse invocations of the macro.

---
+++ prse-derive/src/lib.rs
@@ -0,0 +1,23 @@
+mod parse;
+
+extern crate proc_macro2;
+// #[macro_use]
+// extern crate quote;
+extern crate syn;
+
+use proc_macro::TokenStream;
+
+#[proc_macro]
+pub fn parse(input: TokenStream) -> TokenStream {
+    input
+}
+
+#[proc_macro]
+pub fn try_parse(input: TokenStream) -> TokenStream {
+    input
+}
+
+#[proc_macro]
+pub fn multi_parse(input: TokenStream) -> TokenStream {
+    input
+}
