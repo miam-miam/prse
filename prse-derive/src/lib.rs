41d1fe1fd08f1f96c4843487c39019b696c390c6
Pavloslav <pavloslav@ukr.net>
2023-12-07 13:15:21 +0200

    Update prse-derive/src/lib.rs
    Co-authored-by: miam-miam100 <49870539+miam-miam100@users.noreply.github.com>

--- prse-derive/src/lib.rs
+++ prse-derive/src/lib.rs
@@ -84,7 +84,10 @@ mod var;
 /// assert_eq!(animal_count, 79);
 /// ```
 ///
-/// All three multiple parsers (Array, Vec and Iterator) allow "{:sep:!kind}" syntax to skip multiple separators, for example
+///
+/// ## Modifiers
+///
+/// All three multi-parsers (Array, Vec and Iterator) allow `{<var>:<sep>:!<kind>}` syntax to skip multiple separators, for example
 /// ```ignore
 /// assert_eq!([1, 2, 3], parse!("1-2---3", "{:-:!3}"));
 /// ```
