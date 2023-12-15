64b1f745d7da88a8b13cddd59f9e61782dbd9f20
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2023-12-15 12:51:27 +0000

    Allow ? and .await to be parsed and used inside parse macros. (#31)
    * Allow ? and .await to be parsed and used.
    
    * Update syn and add macro call test

--- tests/test-std/lib.rs
+++ tests/test-std/lib.rs
@@ -183,5 +183,31 @@ mod tests {
         assert_eq!(Some(Ok('@')), iter.next());
     }
 
+    #[test]
+    fn parse_weird_exprs() {
+        async fn async_parsing() -> u32 {
+            let line = async { "5" };
+            parse!(line.await, "{}")
+        }
+
+        fn try_parsing() -> Result<u32, prse::ParseError> {
+            let line: Result<_, prse::ParseError> = Ok("7");
+            try_parse!(line?, "{}")
+        }
+
+        fn try_macro_call(num: u8) -> Result<u32, prse::ParseError> {
+            try_parse!(format!("{num}{}", try_parsing()?), "{}")
+        }
+
+        fn option_parsing() -> Option<u32> {
+            Some(parse!("4\n3".lines().next()?, "{}"))
+        }
+
+        assert_eq!(5, pollster::block_on(async_parsing()));
+        assert_eq!(Ok(7), try_parsing());
+        assert_eq!(Some(4), option_parsing());
+        assert_eq!(Ok(77), try_macro_call(7));
+    }
+
     include!("../common.rs");
 }
